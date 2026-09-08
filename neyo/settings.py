b465f9a14b2b7e93cd3d1571506bc2e869a8601c
harshhanus189 <harsh.hanus189@gmail.com>
2026-09-08 10:23:07 +0530

    changed the neyo to neyo naturals in allowed host

--- neyo/settings.py
+++ neyo/settings.py
@@ -36,7 +36,7 @@ RAZORPAY_KEY_SECRET = "HhSVwgDk47U1MMamz1TZBYDt"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'neyo.com', 'www.neyo.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'neyonaturals.com', 'www.neyonaturals.com']
 
 # Application definition
 INSTALLED_APPS = [
